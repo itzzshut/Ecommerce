192a1076efa62b8dbfb05b816c24955f79cad01d
Mariam <mariammalik5100@gmail.com>
2025-07-23 18:03:05 +0530

    Added futuristic-bg.jpg to public

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -8,14 +8,15 @@ import OrderSummary from "../components/OrderSummary";
 import GiftCouponCard from "../components/GiftCouponCard";
 
 const CartPage = () => {
+	console.log("Inside CartPage");
 	const { cart } = useCartStore();
 
 	return (
 		<div className='py-8 md:py-16'>
-			<div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
+			<div className='max-w-screen-xl px-4 mx-auto 2xl:px-0'>
 				<div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
 					<motion.div
-						className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'
+						className='flex-none w-full mx-auto lg:max-w-2xl xl:max-w-4xl'
 						initial={{ opacity: 0, x: -20 }}
 						animate={{ opacity: 1, x: 0 }}
 						transition={{ duration: 0.5, delay: 0.2 }}
@@ -34,7 +35,7 @@ const CartPage = () => {
 
 					{cart.length > 0 && (
 						<motion.div
-							className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
+							className='flex-1 max-w-4xl mx-auto mt-6 space-y-6 lg:mt-0 lg:w-full'
 							initial={{ opacity: 0, x: 20 }}
 							animate={{ opacity: 1, x: 0 }}
 							transition={{ duration: 0.5, delay: 0.4 }}
@@ -52,16 +53,16 @@ export default CartPage;
 
 const EmptyCartUI = () => (
 	<motion.div
-		className='flex flex-col items-center justify-center space-y-4 py-16'
+		className='flex flex-col items-center justify-center py-16 space-y-4'
 		initial={{ opacity: 0, y: 20 }}
 		animate={{ opacity: 1, y: 0 }}
 		transition={{ duration: 0.5 }}
 	>
-		<ShoppingCart className='h-24 w-24 text-gray-300' />
+		<ShoppingCart className='w-24 h-24 text-gray-300' />
 		<h3 className='text-2xl font-semibold '>Your cart is empty</h3>
 		<p className='text-gray-400'>Looks like you {"haven't"} added anything to your cart yet.</p>
 		<Link
-			className='mt-4 rounded-md bg-emerald-500 px-6 py-2 text-white transition-colors hover:bg-emerald-600'
+			className='px-6 py-2 mt-4 text-white transition-colors rounded-md bg-emerald-500 hover:bg-emerald-600'
 			to='/'
 		>
 			Start Shopping
