8551ef31b3309b56f5d942dac112e9c968190a75
Mariam <mariammalik5100@gmail.com>
2025-07-23 17:51:02 +0530

    chnage in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,16 +4,8 @@ const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5000/api"
-			: import.meta.env.VITE_BACKEND_URL + "/api",
+			: import.meta.env.VITE_BACKEND_URL.replace(/\/$/, "") + "/api",
 	withCredentials: true,
 });
 
-// Intercept requests to remove leading slash from URL
-axiosInstance.interceptors.request.use((config) => {
-	if (config.url.startsWith("/")) {
-		config.url = config.url.substring(1); // remove leading slash
-	}
-	return config;
-});
 
-export default axiosInstance;
